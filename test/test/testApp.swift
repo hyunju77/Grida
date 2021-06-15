125ffa54fd6f4d8bea7aaea41127a93a77568dba
YooTaeJeong <80680728+YooTaeJeong@users.noreply.github.com>
2021-06-15 22:15:46 +0900

    first commit

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by swa on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testexample2/testexample2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testexample2
+//
+//  Created by swa on 2021/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
