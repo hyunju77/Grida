3863d0562d62db5653ff08e8bf7c000efe1a58db
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2021-06-18 11:52:18 +0900

    last

--- Grida/ViewController.swift
+++ Grida/ViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ImageAnimationViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fclass101.net%2Fproducts%2Fz1g4Oml6tr0o7OaCL7iy&psig=AOvVaw2l63Pkk0ZTK2Y-j8FzvFi1&ust=1624067809750000&source=images&cd=vfe&ved=0CAcQjRxqFwoTCOjnmqGKoPECFQAAAAAdAAAAABAK")
+        do {
+            let data = try Data(contentsOf: url!)
+            imageView.image = UIImage(data: data)
+            
+        } catch {
+            
+        }
     }
-
-
 }
 
